3093c722a70a3ac4e3f155d5a0f9e58dc6ae6c9f
shobhit <shobhitsirohi.dev@gmail.com>
2021-09-10 22:15:12 +0530

    first commit of team-page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      inset: {
+        "-18": "-4.2rem",
+        9: "2.25rem",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
